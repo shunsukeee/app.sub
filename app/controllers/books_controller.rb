3885fcdbdf94d159b3a031167baf6d06a498a797
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-15 01:23:54 +0900

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,48 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+  	@book = Book.new
   	@books = Book.all
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
-  	@book = Book.new
-  	@books = Book.all
+
   end
 
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to book_path
+  	redirect_to book_path(book.id)
   end
 
-  private
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+  def update
+	book = Book.find(params[:id])
+	book.update(book_params)
+	redirect_to book_path(book)
+end
+
+def destroy
+	book = Book.find(params[:id])
+	book.destroy
+	redirect_to book_path
+end
+
+private
   def book_params
   	params.require(:book).permit(:title, :body)
   end
 
-  def edit
-  end
+
+
+
 end
