2e16572df31103a58548a4822363b1058dfa27c6
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-14 20:51:02 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+  	@books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+  def edit
+  end
+end
